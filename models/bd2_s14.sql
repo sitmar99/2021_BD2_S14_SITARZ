ec9432c6ba1361808fcb5fd72a4f2979a3a68c58
Błażej Hanzel <blazej.hanzel@gmail.com>
2021-04-28 21:13:46 +0200

    model bazy danych

---
+++ models/bd2_s14.sql
@@ -0,0 +1,213 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 28 Kwi 2021, 21:12
+-- Wersja serwera: 10.4.17-MariaDB
+-- Wersja PHP: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `bd2_s14`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `registry`
+--
+
+CREATE TABLE `registry` (
+  `id` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `user` int(11) NOT NULL,
+  `plate_number` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `registry-resources`
+--
+
+CREATE TABLE `registry-resources` (
+  `id` int(11) NOT NULL,
+  `registry_id` int(11) NOT NULL,
+  `resource_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `registry-services`
+--
+
+CREATE TABLE `registry-services` (
+  `id` int(11) NOT NULL,
+  `registry_id` int(11) NOT NULL,
+  `service_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `resources`
+--
+
+CREATE TABLE `resources` (
+  `id` int(11) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `quantity` float NOT NULL,
+  `unit` varchar(8) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `services`
+--
+
+CREATE TABLE `services` (
+  `id` int(11) NOT NULL,
+  `parent` int(11) DEFAULT NULL,
+  `name` varchar(64) NOT NULL,
+  `price` decimal(6,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `role` varchar(16) NOT NULL,
+  `salary` decimal(6,2) DEFAULT NULL COMMENT 'wypłata miesięczna'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `registry`
+--
+ALTER TABLE `registry`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user` (`user`);
+
+--
+-- Indeksy dla tabeli `registry-resources`
+--
+ALTER TABLE `registry-resources`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `registry_id` (`registry_id`),
+  ADD KEY `resource_id` (`resource_id`);
+
+--
+-- Indeksy dla tabeli `registry-services`
+--
+ALTER TABLE `registry-services`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `registry_id` (`registry_id`),
+  ADD KEY `service_id` (`service_id`);
+
+--
+-- Indeksy dla tabeli `resources`
+--
+ALTER TABLE `resources`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `registry`
+--
+ALTER TABLE `registry`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `registry-resources`
+--
+ALTER TABLE `registry-resources`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `registry-services`
+--
+ALTER TABLE `registry-services`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `resources`
+--
+ALTER TABLE `resources`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `services`
+--
+ALTER TABLE `services`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `registry`
+--
+ALTER TABLE `registry`
+  ADD CONSTRAINT `registry_ibfk_1` FOREIGN KEY (`user`) REFERENCES `users` (`id`);
+
+--
+-- Ograniczenia dla tabeli `registry-resources`
+--
+ALTER TABLE `registry-resources`
+  ADD CONSTRAINT `registry-resources_ibfk_1` FOREIGN KEY (`registry_id`) REFERENCES `registry` (`id`),
+  ADD CONSTRAINT `registry-resources_ibfk_2` FOREIGN KEY (`resource_id`) REFERENCES `resources` (`id`);
+
+--
+-- Ograniczenia dla tabeli `registry-services`
+--
+ALTER TABLE `registry-services`
+  ADD CONSTRAINT `registry-services_ibfk_1` FOREIGN KEY (`registry_id`) REFERENCES `registry` (`id`),
+  ADD CONSTRAINT `registry-services_ibfk_2` FOREIGN KEY (`service_id`) REFERENCES `services` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
