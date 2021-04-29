562d4ba233a5fb1cb2b63e2f4914b81901102e40
Błażej Hanzel <blazej.hanzel@gmail.com>
2021-04-29 23:36:47 +0200

    formularz logowania (html)

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,7 +1,15 @@
 import Navbar from './components/Navbar';
+import LoginForm from './components/LoginForm';
 
 function App() {
-  return <Navbar />;
+    return (
+        <div id="app">
+            <Navbar />
+            <div class="container">
+                <LoginForm />
+            </div>
+        </div>
+    )
 }
 
 export default App;
