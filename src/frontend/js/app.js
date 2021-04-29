f951af64fed60c29a4750ec7ea30b0dbf226ecad
Błażej Hanzel <blazej.hanzel@gmail.com>
2021-04-29 18:14:55 +0200

    npm server + hello world navbar

--- src/frontend/js/app.js
+++
@@ -1,8 +0,0 @@
-class App extends React.Component {
-    render() {
-        return React.createElement('p', null, "Hello World");
-    }
-}
-
-const domContainer = document.querySelector('#app');
-ReactDOM.render(React.createElement(App), domContainer);
