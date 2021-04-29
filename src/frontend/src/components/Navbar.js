c45485295b0b7fd179eb661376eaca37ccbdd9cc
Błażej Hanzel <blazej.hanzel@gmail.com>
2021-04-29 18:30:23 +0200

    npm server updated and cleaned

--- src/frontend/src/components/Navbar.js
+++ src/frontend/src/components/Navbar.js
@@ -4,7 +4,7 @@ class Navbar extends React.Component {
     render() {
         return (
             <nav className="navbar navbar-light bg-light">
-                <span className="navbar-brand mb-0 h1">Navbar</span>
+                <span className="navbar-brand mb-0 h1">Hello Navbar</span>
             </nav>
         )
     }
