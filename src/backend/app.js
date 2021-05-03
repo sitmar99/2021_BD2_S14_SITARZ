4fb8e8219f129c7f5bd4f4d130f9d77973a73d17
Błażej Hanzel <blazej.hanzel@gmail.com>
2021-05-03 12:24:47 +0200

    połączenie serwera express z serwerem mysql

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,8 +1,24 @@
+// to są takie #include<..>
 const express = require('express')
+const mysql = require('mysql')
+
+// stałe definiujące parametry połączenia
 const hostname = '127.0.0.1'
 const port = 8080
 
+// zmienne serwera i połączenia z mysql
 const app = express()
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: ""
+})
+
+// połączenie z serwerem mysql
+con.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to mysql server...')
+})
 
 app.get('/', (req, res) => {
     res.statusCode = 200;
