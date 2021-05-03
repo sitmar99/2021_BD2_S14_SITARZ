07c27b669ed631dffa728dec9d1fc454cdb18e65
Błażej Hanzel <blazej.hanzel@gmail.com>
2021-05-03 13:28:29 +0200

    połączenie z bazą danych i routingi oddzielone do osobnych plików

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,45 +1,24 @@
 // to są takie #include<..>
 const express = require('express')
-const mysql = require('mysql')
+const con = require('./modules/database')
 const bodyParser = require('body-parser')
 
 // stałe definiujące parametry połączenia
 const hostname = '127.0.0.1'
 const port = 8080
 
-// zmienne serwera i połączenia z mysql
+// zmienne serwera
 const app = express()
 app.use(bodyParser.json()) // body będą przekazywane jako JSON
-const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "bd2_s14"
-})
 
-// połączenie z serwerem mysql
-con.connect((err) => {
-    if (err) throw err;
-    console.log('Connected to mysql server...')
-})
+// zmienne routingów
+var loginRoutes = require('./routes/login')
+app.use('/login', loginRoutes)
 
 app.get('/', (req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.send('Hello node')
-})
-
-app.post('/login', (req, res) => {
-    con.query(`SELECT * FROM users WHERE username = '${req.body.login}';`, (err, result) => {
-        if (err) throw err;
-
-        if (result.length > 0 && result[0].password === req.body.password) {
-            res.sendStatus(200);
-        } else {
-            res.statusCode = 401;
-            res.send('User doesn\'t exists or wrong password')
-        }
-    })
+    res.send('Hello node API!')
 })
 
 app.listen(port, hostname, () => {
---
+++ src/backend/routes/login.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const con = require('./../modules/database')
+var router = express.Router()
+
+router.post('/', (req, res) => {
+    con.query(`SELECT * FROM users WHERE username = '${req.body.login}';`, (err, result) => {
+        if (err) throw err;
+
+        if (result.length > 0 && result[0].password === req.body.password) {
+            res.sendStatus(200);
+        } else {
+            res.statusCode = 401;
+            res.send('User doesn\'t exists or wrong password')
+        }
+    })
+})
+
+module.exports = router
