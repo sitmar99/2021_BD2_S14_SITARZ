95fe66788feaa9e3d03357be8267c054d659ec8e
Błażej Hanzel <blazej.hanzel@gmail.com>
2021-05-03 12:07:29 +0200

    node.js server hello world przeportowany na express

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,14 +1,15 @@
-const http = require('http');
+const express = require('express')
+const hostname = '127.0.0.1'
+const port = 8080
 
-const hostname = '127.0.0.1';
-const port = 8080;
+const app = express()
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+app.get('/', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Hello node')
+})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
