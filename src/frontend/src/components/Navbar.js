048c1598926e4b9b2e1e85e9b7fc87495af9d8d2
Kamil Marek <kamil.a.marek@gmail.com>
2021-05-11 15:14:41 +0200

    zmieniono nazwę studia

--- src/frontend/src/components/Navbar.js
+++ src/frontend/src/components/Navbar.js
@@ -4,7 +4,7 @@ class Navbar extends React.Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <span className="navbar-brand mb-0 h1">Studio samochodowe bez nazwy</span>
+                <span className="navbar-brand mb-0 h1">Ride It - detailing studio</span>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
