e751479219c72e7948c68e39faa6c3572b9e5b7c
sitmar <sitmar@poczta.fm>
2021-05-12 19:41:58 +0200

    Listuje pracowników

--- src/frontend/src/components/EmploeeList.js
+++ src/frontend/src/components/EmploeeList.js
@@ -1,31 +1,37 @@
 import React from 'react';
 
 class EmploeeList extends React.Component {
-    render() {
-        return (
-            <div id="emploeeList">
-            <div class="row-12 mt-2 justify-content-center">
-                <button type="button" class="btn btn-block btn-info">Dodaj pracownika</button>
-            </div>
-            <div class="row-12 mt-2 justify-content-center">
-                <div class="list-group">
-                    <a href="#" class="list-group-item list-group-item-action" aria-current="true">
+    
+    constructor(props) {
+        super (props)
+        var json = JSON.parse('[{"id":1, "active":false, "username": "lesne_ruchadlo", "role": "pracownik", "first_name": "Adam", "last_name": "Cienkopis"}]')
+        
+        this.state = {
+            employees: json
+        }
+    }
+
+    generate() {
+        var tab = []
+        for (const employee of this.state.employees) {
+            tab.push(
+                <a href="#" class="list-group-item list-group-item-action" aria-current="true">
                         <div class="d-flex w-100 justify-content-between">
                             <div class="col-9">
                                 <div class="row">
-                                    <h5>USER_ID</h5>
+                                    <h5>USER_ID: {employee.id} </h5>
                                 </div>
                                 <div class="row">
-                                    <h4>USER_NAME</h4>
+                                    <h4>USER_NAME: {employee.username}</h4>
                                 </div>
                                 <div class="row">
-                                    <h6>ROLE</h6>
+                                    <h6>ROLE: {employee.role}</h6>
                                 </div>
                                 <div class="row">
-                                    <h6>FIRST_NAME</h6>
+                                    <h6>FIRST_NAME: {employee.first_name}</h6>
                                 </div>
                                 <div class="row">
-                                    <h6>LAST_NAME</h6>
+                                    <h6>LAST_NAME: {employee.last_name}</h6>
                                 </div>
                             </div>
                             <div class="col-3">
@@ -34,12 +40,31 @@ class EmploeeList extends React.Component {
                                         <button type="button" class="btn btn-success">Edytuj</button>
                                     </div>
                                     <div class="col">
-                                        <button type="button" class="btn btn-danger">Dezaktywuj</button>
+                                        {(() => {
+                                            if (employee.active)
+                                                return <button type="button" class="btn btn-danger">Dezaktywuj</button>
+                                            else
+                                                return <button type="button" class="btn btn-danger">Aktywuj</button>
+                                            })()}
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </a>
+            )
+        }
+        return tab
+    }
+
+    render() {
+        return (
+            <div id="emploeeList">
+            <div class="row-12 mt-2 justify-content-center">
+                <button type="button" class="btn btn-block btn-info">Dodaj pracownika</button>
+            </div>
+            <div class="row-12 mt-2 justify-content-center">
+                <div class="list-group">
+                    {this.generate()}
                 </div>
             </div>
             </div>
