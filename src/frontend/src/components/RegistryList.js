2a7fb49c0b4d2029388353ac4fb1e2f1c6f21686
sitmar <sitmar@poczta.fm>
2021-05-12 23:09:14 +0200

    check if completed

--- src/frontend/src/components/RegistryList.js
+++ src/frontend/src/components/RegistryList.js
@@ -4,43 +4,49 @@ class EmploeeList extends React.Component {
     
     constructor(props) {
         super (props)
-        var json = JSON.parse('[{"id":1, "date": "12-05-2021", "first_name": "Ryszard", "last_name": "Sanchez", "plate_number": "WA 717B", "price": 130}]')
+        var json = JSON.parse('[{"id":1, "completed": true, "date": "12-05-2021", "first_name": "Ryszard", "last_name": "Sanchez", "plate_number": "WA 717B", "price": 130},{"id":2, "completed": false, "date": "12-07-2031", "first_name": "Ryszard", "last_name": "Sanchez", "plate_number": "WA 717A", "price": 230}]')
         
         this.state = {
             services: json
         }
     }
 
+    checkComplete(completed) {
+        if (completed)
+            return ""
+        return "active"
+    }
+
     generate() {
         var tab = []
         for (const service of this.state.services) {
             tab.push(
-                <a href="#" class="list-group-item list-group-item-action" aria-current="true">
-                        <div class="d-flex w-100 justify-content-between">
-                            <div class="col-9">
-                                <div class="row">
-                                    <h5>Service id: {service.id} </h5>
-                                </div>
-                                <div class="row">
-                                    <h4>Date: {service.date}</h4>
-                                </div>
-                                <div class="row">
-                                    <h5>First name: {service.first_name} </h5>
-                                </div>
-                                <div class="row">
-                                    <h6>Last name: {service.last_name}</h6>
-                                </div>
-                                <div class="row">
-                                    <h6>Plate number: {service.plate_number}</h6>
-                                </div>
+                <a id="lista" href="#" class={"list-group-item list-group-item-action " + this.checkComplete(service.completed)} aria-current="true">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="col-9">
+                            <div class="row">
+                                <h5>Service id: {service.id} </h5>
                             </div>
-                            <div className="row-3">
-                                <div className="col">
-                                    <h3>Cena: {service.price}zł</h3>
-                                </div>
+                            <div class="row">
+                                <h4>Date: {service.date}</h4>
+                            </div>
+                            <div class="row">
+                                <h5>Last name: {service.last_name}</h5>
+                            </div>
+                            <div class="row">
+                                <h6>First name: {service.first_name} </h6>
+                            </div>
+                            <div class="row">
+                                <h6>Plate number: {service.plate_number}</h6>
                             </div>
                         </div>
-                    </a>
+                        <div className="row-3">
+                            <div className="col">
+                                <h3>Cena: {service.price}zł</h3>
+                            </div>
+                        </div>
+                    </div>
+                </a>
             )
         }
         return tab
