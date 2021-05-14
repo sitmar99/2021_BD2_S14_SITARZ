d1d2fbdc5b47d52db59fd907732d5adaccbf375f
sitmar <sitmar@poczta.fm>
2021-05-14 18:13:16 +0200

    edit emploee modal

--- src/frontend/src/components/EmploeeList.js
+++ src/frontend/src/components/EmploeeList.js
@@ -4,7 +4,7 @@ class EmploeeList extends React.Component {
     
     constructor(props) {
         super (props)
-        var json = JSON.parse('[{"id":1, "active":true, "username": "lesnik", "role": "pracownik", "first_name": "Andżej", "last_name": "Cienkopis"}, {"id":2, "active":false, "username": "prezes", "role": "admin", "first_name": "Czaruś", "last_name": "Fiona"}]')
+        var json = JSON.parse('[{"id":1, "active":true, "username": "lesnik", "role": "pracownik", "first_name": "Andżej", "last_name": "Cienkopis", "salary":2800}, {"id":2, "active":false, "username": "prezes", "role": "admin", "first_name": "Czaruś", "last_name": "Fiona", "salary":280000}]')
         
         this.state = {
             employees: json
@@ -15,6 +15,66 @@ class EmploeeList extends React.Component {
         var tab = []
         for (const employee of this.state.employees) {
             tab.push(
+                // edit emploee modal
+                <div id="single emploee">
+                <div class="modal fade" id={"editEmploee" + employee.id} tabindex="-1" aria-labelledby="editEmploee" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="editEmploee">Edit emploee</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <form>
+                        <div class="modal-body">
+                            <div class="form-outline">
+                                <label for="username">Username</label>
+                                <input type="text" class="form-control" id="username" defaultValue={employee.username}></input>
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" class="form-control" id="password"></input>
+                            </div>
+                            <div className="form-group">
+                                <label for="role">Role</label>
+                                <select id="role" class="form-control">
+                                <option>Emploee</option>
+                                <option>Manager</option>
+                                <option>Administrator</option>
+                                </select>
+                            </div>
+                            <div class="form-row mb-3">
+                                <div class="col">
+                                    <label for="firstName">First name</label>
+                                    <input id="firstName" type="text" class="form-control" defaultValue={employee.first_name}></input>
+                                </div>
+                                <div class="col">
+                                    <label for="lastName">Last name</label>
+                                    <input id="lastName" type="text" class="form-control" defaultValue={employee.last_name}></input>
+                                </div>
+                            </div>
+                            <div class="form-row">
+                                <div class="col mr-5">
+                                    <label for="salary">Salary</label>
+                                    <input id="salary" type="number" class="form-control" defaultValue={employee.salary}></input>
+                                </div>
+                                <div class="col">
+                                    <input type="checkbox" class="form-check-input" id="active" checked></input>
+                                    <label class="form-check-label" for="active">Active</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
+                    </div>
+                </div>
+                </div>
+
+                {/* single emploee card */}
                 <a href="#" class="list-group-item list-group-item-action" aria-current="true">
                         <div class="d-flex w-100 justify-content-between">
                             <div class="col-9">
@@ -40,7 +100,7 @@ class EmploeeList extends React.Component {
                             <div class="col-3">
                                 <div class="row">
                                     <div class="col">
-                                        <button type="button" class="btn btn-success">Edytuj</button>
+                                        <button type="button" class="btn btn-success" data-toggle="modal" data-target={"#editEmploee" + employee.id}>Edytuj</button>
                                     </div>
                                     <div class="col">
                                         {(() => {
@@ -54,6 +114,7 @@ class EmploeeList extends React.Component {
                             </div>
                         </div>
                     </a>
+                </div>
             )
         }
         return tab
@@ -69,28 +130,28 @@ class EmploeeList extends React.Component {
             </div>
 
             {/* new employee modal */}
-            <div class="modal fade" id="newEmploee" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal fade" id="newEmploee" tabindex="-1" aria-labelledby="newEmploee" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">New emploee</h5>
+                        <h5 class="modal-title" id="newEmploee">New emploee</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
                     <form>
                         <div class="modal-body">
-                            <div class="form-group">
+                            <div class="form-outline">
                                 <label for="username">Username</label>
                                 <input type="text" class="form-control" id="username"></input>
                             </div>
                             <div class="form-group">
-                                <label for="exampleInputPassword1">Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1"></input>
+                                <label for="password">Password</label>
+                                <input type="password" class="form-control" id="password"></input>
                             </div>
                             <div className="form-group">
                                 <label for="role">Role</label>
-                                <select class="form-control">
+                                <select id="role" class="form-control">
                                 <option>Emploee</option>
                                 <option>Manager</option>
                                 <option>Administrator</option>
@@ -121,7 +182,7 @@ class EmploeeList extends React.Component {
                     </form>
                     </div>
                 </div>
-                </div>
+            </div>
 
             {/* emploees list */}
             <div class="row-12 mt-2 justify-content-center">
