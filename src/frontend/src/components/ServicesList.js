ee17c46b65106c38485274e14fa9247a6dce15ca
sitmar <sitmar@poczta.fm>
2021-05-16 00:35:01 +0200

    new service and edit service modals

--- src/frontend/src/components/ServicesList.js
+++ src/frontend/src/components/ServicesList.js
@@ -5,26 +5,25 @@ class ServicesList extends React.Component {
     constructor(props) {
         super (props)
         var json = JSON.parse(`[
-            { "id": 1, "active": true, "child": 0, "name": "Pierwsza usługa" },
+            { "id": 1, "active": false, "child": 0, "name": "Pierwsza usługa", "price": 150 },
             {
               "id": 2,
               "active": true,
               "child":
                 [
-                  { "id": 3, "active": true, "child": 0, "name": "Pierwsza pod-usługa" },
-                  { "id": 4, "active": true, "child": 0, "name": "Druga pod-usługa" },
+                  { "id": 3, "active": true, "child": 0, "name": "Pierwsza pod-usługa", "price": 25 },
+                  { "id": 4, "active": true, "child": 0, "name": "Druga pod-usługa", "price": 50 },
                   { "id": 5, "active": true, "child": 
                     [
                         { "id": 6, "active": true, "child":
                             [
-                                { "id": 7, "active": true, "child": 0, "name": "Pierwsza pod-pod-pod-usługa" }
+                                { "id": 7, "active": true, "child": 0, "name": "Pierwsza pod-pod-pod-usługa", "price": 250 }
                             ], 
-                            "name": "Pierwsza pod-pod-usługa" }
+                            "name": "Pierwsza pod-pod-usługa", "price": 0 }
                     ], 
-                    "name": "Trzecia pod-usługa" }
+                    "name": "Trzecia pod-usługa", "price": 0 }
                 ],
-                "name": "Druga usługa"
-            }
+                "name": "Druga usługa", "price": 0 }
         ]`)
         
         this.state = {
@@ -35,14 +34,68 @@ class ServicesList extends React.Component {
     generate(services) {
         var tab = []
         for (const service of services) {
+
+            // childless service
             if (service.child == 0) {
                 tab.push (
-                    <div class="card-body">
-                        {service.name}
+                    <div>
+                    {/* single service */}
+                    <div class="row card-body">
+                        <div className="col-9">
+                            {service.name}
+                            <br></br>
+                            Cena: {service.price}
+                        </div>
+                        <div className="col-3 text-right">
+                            <div class="row justify-content-end">
+                                <button type="buttont" class="btn btn-success text-right" data-toggle="modal" data-target={"#editService" + service.id}>Edytuj</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* edit service modal */}
+                    <div class="modal fade" id={"editService"+service.id} tabindex="-1" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Edit service</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <form>
+                            <div class="modal-body">
+                                <div class="form-outline">
+                                    <label for="name">Name</label>
+                                    <input type="text" class="form-control" id="name" defaultValue={service.name}></input>
+                                </div>
+                                <div class="form-group">
+                                    <label for="parentID">Parent ID</label>
+                                    <input type="number" class="form-control" id="parentID"></input>
+                                </div>
+                                <div class="form-row">
+                                    <div class="col mr-5">
+                                        <input type="number" class="form-control" defaultValue={service.price}></input>
+                                    </div>
+                                    <div class="col">
+                                        <input type="checkbox" class="form-check-input" id="active" checked={(() => {if (service.active) return "checked"})()}></input>
+                                        <label class="form-check-label" for="active">Active</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                        </form>
+                        </div>
+                    </div>
+                    </div>
                     </div>
                 )
             }
 
+            // services with childs
             else {
                 tab.push (
                     <div>
@@ -56,7 +109,7 @@ class ServicesList extends React.Component {
 
                         <div id={"collapse"+service.id} class="collapse collapsed">
                             <div class="card-body">
-                                {this.createAcordion(service.child)}
+                                {this.generate(service.child)}
                             </div>
                         </div>
                     </div>
@@ -72,8 +125,48 @@ class ServicesList extends React.Component {
 
             {/* new service button */}
             <div class="row-12 mt-2 justify-content-center">
-                <button type="button" class="btn btn-block btn-info" data-toggle="modal" data-target="#newEmploee">New emploee</button>
+                <button type="button" class="btn btn-block btn-info" data-toggle="modal" data-target="#newService">New service</button>
             </div>
+
+            {/* new service modal */}
+            <div class="modal fade" id="newService" tabindex="-1" aria-labelledby="newService" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">New service</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <form>
+                        <div class="modal-body">
+                            <div class="form-outline">
+                                <label for="name">Name</label>
+                                <input type="text" class="form-control" id="name"></input>
+                            </div>
+                            <div class="form-group">
+                                <label for="parentID">Parent ID</label>
+                                <input type="number" class="form-control" id="parentID"></input>
+                            </div>
+                            <div class="form-row">
+                                <div class="col mr-5">
+                                    <input type="number" class="form-control" placeholder="Price"></input>
+                                </div>
+                                <div class="col">
+                                    <input type="checkbox" class="form-check-input" id="active" checked></input>
+                                    <label class="form-check-label" for="active">Active</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
+                    </div>
+                </div>
+            </div>
+
 
             {/* services list */}
             <div class="row-12 mt-2 justify-content-center">
