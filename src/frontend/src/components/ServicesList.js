09f051a099a8b9c5316ddfbbda2de6266f6e4b45
sitmar <sitmar@poczta.fm>
2021-05-16 00:48:55 +0200

    Edit services modal & parent id int json

--- src/frontend/src/components/ServicesList.js
+++ src/frontend/src/components/ServicesList.js
@@ -5,19 +5,19 @@ class ServicesList extends React.Component {
     constructor(props) {
         super (props)
         var json = JSON.parse(`[
-            { "id": 1, "active": false, "child": 0, "name": "Pierwsza usługa", "price": 150 },
+            { "id": 1, "active": false, "parent": 0, "child": 0, "name": "Pierwsza usługa", "price": 150 },
             {
               "id": 2,
               "active": true,
               "child":
                 [
-                  { "id": 3, "active": true, "child": 0, "name": "Pierwsza pod-usługa", "price": 25 },
-                  { "id": 4, "active": true, "child": 0, "name": "Druga pod-usługa", "price": 50 },
-                  { "id": 5, "active": true, "child": 
+                  { "id": 3, "active": true, "parent": 2, "child": 0, "name": "Pierwsza pod-usługa", "price": 25 },
+                  { "id": 4, "active": true, "parent": 2, "child": 0, "name": "Druga pod-usługa", "price": 50 },
+                  { "id": 5, "active": true, "parent": 2, "child": 
                     [
-                        { "id": 6, "active": true, "child":
+                        { "id": 6, "active": true, "parent": 5, "child":
                             [
-                                { "id": 7, "active": true, "child": 0, "name": "Pierwsza pod-pod-pod-usługa", "price": 250 }
+                                { "id": 7, "active": true, "parent": 6, "child": 0, "name": "Pierwsza pod-pod-pod-usługa", "price": 250 }
                             ], 
                             "name": "Pierwsza pod-pod-usługa", "price": 0 }
                     ], 
@@ -34,6 +34,47 @@ class ServicesList extends React.Component {
     generate(services) {
         var tab = []
         for (const service of services) {
+            
+            // edit service modal
+            tab.push (
+                <div class="modal fade" id={"editService"+service.id} tabindex="-1" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Edit service</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <form>
+                        <div class="modal-body">
+                            <div class="form-outline">
+                                <label for="name">Name</label>
+                                <input type="text" class="form-control" id="name" defaultValue={service.name}></input>
+                            </div>
+                            <div class="form-group">
+                                <label for="parentID">Parent ID</label>
+                                <input type="number" class="form-control" id="parentID" defaultValue={service.parent}></input>
+                            </div>
+                            <div class="form-row">
+                                <div class="col mr-5">
+                                    <input type="number" class="form-control" defaultValue={service.price}></input>
+                                </div>
+                                <div class="col">
+                                    <input type="checkbox" class="form-check-input" id="active" checked={(() => {if (service.active) return "checked"})()}></input>
+                                    <label class="form-check-label" for="active">Active</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
+                    </div>
+                </div>
+                </div>
+            )
 
             // childless service
             if (service.child == 0) {
@@ -42,8 +83,8 @@ class ServicesList extends React.Component {
                     {/* single service */}
                     <div class="row card-body">
                         <div className="col-9">
-                            {service.name}
-                            <br></br>
+                            ID: {service.id}
+                            <h5><b>{service.name}</b></h5>
                             Cena: {service.price}
                         </div>
                         <div className="col-3 text-right">
@@ -51,45 +92,6 @@ class ServicesList extends React.Component {
                                 <button type="buttont" class="btn btn-success text-right" data-toggle="modal" data-target={"#editService" + service.id}>Edytuj</button>
                             </div>
                         </div>
-                    </div>
-
-                    {/* edit service modal */}
-                    <div class="modal fade" id={"editService"+service.id} tabindex="-1" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title">Edit service</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <form>
-                            <div class="modal-body">
-                                <div class="form-outline">
-                                    <label for="name">Name</label>
-                                    <input type="text" class="form-control" id="name" defaultValue={service.name}></input>
-                                </div>
-                                <div class="form-group">
-                                    <label for="parentID">Parent ID</label>
-                                    <input type="number" class="form-control" id="parentID"></input>
-                                </div>
-                                <div class="form-row">
-                                    <div class="col mr-5">
-                                        <input type="number" class="form-control" defaultValue={service.price}></input>
-                                    </div>
-                                    <div class="col">
-                                        <input type="checkbox" class="form-check-input" id="active" checked={(() => {if (service.active) return "checked"})()}></input>
-                                        <label class="form-check-label" for="active">Active</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="submit" class="btn btn-primary">Submit</button>
-                            </div>
-                        </form>
-                        </div>
-                    </div>
                     </div>
                     </div>
                 )
@@ -99,12 +101,18 @@ class ServicesList extends React.Component {
             else {
                 tab.push (
                     <div>
-                        <div class="card-header" id={"heading"+service.id}>
-                            <h2 class="mb-0">
+                        <div class="row card-header">
+                            <div className="col-9">
+                                ID: {service.id}
                                 <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target={"#collapse"+service.id} aria-expanded="true">
-                                    {service.name}
+                                    <h5><b>{service.name}</b></h5>
                                 </button>
-                            </h2>
+                            </div>
+                            <div className="col-3 text-right">
+                            <div class="row justify-content-end">
+                                <button type="buttont" class="btn btn-success text-right" data-toggle="modal" data-target={"#editService" + service.id}>Edytuj</button>
+                            </div>
+                            </div>
                         </div>
 
                         <div id={"collapse"+service.id} class="collapse collapsed">
