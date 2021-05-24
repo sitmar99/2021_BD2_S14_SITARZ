8b3e1ed300dcb637c4add868f4fbb4dc37253bb5
Kamil Marek <kamil.a.marek@gmail.com>
2021-05-24 04:14:11 +0200

    added working JSON

--- src/backend/routes/ServicesList.js
+++ src/backend/routes/ServicesList.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const con = require('./../modules/database')
 const connection = require('./../modules/database')
 var router = express.Router()
 var session = null
@@ -35,50 +36,60 @@ router.get('/', (req, res) => {
     connection.query('SELECT * FROM prices p RIGHT JOIN services s ON s.id = p.service_id', (err,result)=> {
         if (err) throw err
         var all_results = result
-        var one_result = result[0]
-        var one_result_string_old = JSON.stringify(one_result)
+        var all_new_results = '{ "array" : [\n'
         let size = all_results.length
-        for (let i = 0; i < size; i++)
+        for (let i = 5; i < size; i++)
         {
             var one_result_string = JSON.stringify(result[i]) //wyłapanie poszczególnych rekordów
             var columns = one_result_string.indexOf("id") // znalezienie id
-            //console.log(columns)
             var ends = one_result_string.indexOf(",") // znalezienie końca id
-            var one_final_result = one_result_string.substring(0, columns + 4) // dodanie "id":
-            one_final_result += one_result_string.substring(columns + 4, ends + 1) // dodanie konkretnej wartości id
+            var one_final_result = '{ ' + one_result_string.substring(1, columns + 4) // dodanie "id":
+            one_final_result += '\"' + one_result_string.substring(columns + 4, ends)// dodanie konkretnej wartości id
+            one_final_result += '\",'
             columns = one_result_string.indexOf("active") // znalezienie active
             one_final_result += one_result_string.substring(columns - 1, columns + 8) // dodanie "active":
             if (one_result_string[columns + 8] == '1') // sprawdzenie czy usługa jest aktywna i dodanie odpowiedniej wartości
             {
-                one_final_result += 'true,'
+                one_final_result += '\"true\",'
             }
             else
             {
-                one_final_result += 'false,'
+                one_final_result += '\"false\",'
             }
             columns = one_result_string.indexOf("parent")
             one_final_result += one_result_string.substring(columns - 1, columns + 8)
             ends = one_result_string.indexOf(",", columns)
             if (one_result_string.substring(columns + 8, columns + 12) == 'null')
             {
-                one_final_result += '0, \"child\": '
+                one_final_result += '0,\"child\":'
             }
             else
             {
-                one_final_result += one_result_string.substring(columns + 8, ends + 1)
-                one_final_result += "\"child\":0,"
+                one_final_result += '\"' + one_result_string.substring(columns + 8, ends)
+                one_final_result += '\",'
+                one_final_result += "\"child\":\"0\","
             }
             columns = one_result_string.indexOf("name")
             ends = one_result_string.indexOf("}", columns)
-            one_final_result += one_result_string.substring(columns - 1, ends + 1)
-            
-            one_final_result = one_final_result.replaceAll(":", ": ")
-            one_final_result = one_final_result.replaceAll(",", ", ")
-            console.log(one_final_result)
-            //console.log(one_result_string_new.substring(position_parent, position_parent + 10))
+            one_final_result += one_result_string.substring(columns - 1, ends)
+            one_final_result += ","
+            columns = one_result_string.indexOf("price")
+            ends = one_result_string.indexOf(",", columns)
+            one_final_result += one_result_string.substring(columns - 1, columns + 7)
+            one_final_result += "\"" + one_result_string.substring(columns + 7, ends)
+            one_final_result += "\" }"
+            one_final_result = one_final_result.replaceAll(",", " , ")
+            one_final_result = one_final_result.replaceAll("}", "},")
+            //console.log(one_final_result)
+            all_new_results += one_final_result + '\n'
         }
-        //res.send(size + '')
-        res.send(JSON.stringify(result[0]))
+        all_new_results = all_new_results.slice(0, all_new_results.length - 2)
+        all_new_results += '\n]}'
+        console.log(all_new_results)
+        //res.send(result)
+        var new_result = JSON.parse(all_new_results)
+        res.send(new_result)
+        
         
         return
     })
