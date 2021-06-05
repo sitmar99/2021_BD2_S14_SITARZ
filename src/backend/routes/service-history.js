b04a98e6aa2eb342bf9cb0d7a06c4f23b6d04e10
wojcpaw632 <wojcpaw632@student.polsl.pl>
2021-06-05 21:34:17 +0200

    json works

--- src/backend/routes/service-history.js
+++ src/backend/routes/service-history.js
@@ -20,11 +20,43 @@ var router = express.Router()
 router.get('/', (req, res) => {
     if (!hasRole('employee', req, res)) return
 
-    connection.query('SELECT DISTINCT r.id, r.completed, r.date, users.first_name, users.last_name, r.plate_number, prices.price FROM registry r JOIN users ON r.user=users.id JOIN registry_services rs ON r.id=rs.registry_id JOIN prices ON prices.service_id=rs.service_id', (err,result,fields)=> {
+    connection.query('SELECT DISTINCT r.id, r.completed, r.date, users.first_name, users.last_name, r.plate_number, prices.price, services.name FROM registry r JOIN users ON r.user=users.id JOIN registry_services rs ON r.id=rs.registry_id JOIN prices ON prices.service_id=rs.service_id JOIN services ON rs.service_id=services.id', (err,result,fields)=> {
         if (err) throw err
         
-        res.send(result)
-        return
+      var all_results = result
+      
+      let size = all_results.length
+     for (let i = 0; i < size; i++)
+      {
+          var details = []
+          var tmp = {"price": all_results[i].price}
+            tmp.name = ( all_results[i].name)
+            details.push(tmp)
+          for (let j = i + 1; j < size; j++)
+          {   
+              if (all_results[j].id == all_results[i].id)
+              {
+                  var temp = {"price": all_results[j].price}
+                  temp.name = ( all_results[j].name)
+                  details.push(temp)
+                  all_results.splice(j, 1)
+                  size--
+                  //delete all_results[j]
+                  //size--
+              }
+                         
+          }
+          all_results[i] = { ...all_results[i], details}
+      }
+
+    for (let i = 0; i < size; i++) {
+        delete all_results[i].price
+        delete all_results[i].name
+    }
+
+
+        res.send(all_results)
+        //res.send(new Set (all_results))
     })
 })
 
