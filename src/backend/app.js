4d321228633e4364b8c6cfed7ec067c57c7f6025
Błażej Hanzel <blazej.hanzel@gmail.com>
2021-06-21 19:21:25 +0200

    poprawione literówki

--- src/backend/app.js
+++ src/backend/app.js
@@ -53,5 +53,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at port ${port}`);
 })
--- src/backend/modules/database.js
+++ src/backend/modules/database.js
@@ -5,7 +5,7 @@ const options = {
     port: 3306,
     user: "li9v6knw9wi9r2s4",
     password: "zbd8x3l95o68os90",
-    database: "	v7264mir5vsp5sij",
+    database: "v7264mir5vsp5sij",
     multipleStatements: true
 }
 
