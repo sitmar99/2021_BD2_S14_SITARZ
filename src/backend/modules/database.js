88b4dc0f0dba560c4c0551d18b7f79c037f58450
Błażej Hanzel <blazej.hanzel@gmail.com>
2021-06-21 20:17:28 +0200

    przywrócenie bazy danych

--- src/backend/modules/database.js
+++ src/backend/modules/database.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql')
 
 const options = {
-    host: "eporqep6b4b8ql12.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
+    host: "h2.hitme.pl",
     port: 3306,
-    user: "li9v6knw9wi9r2s4",
-    password: "zbd8x3l95o68os90",
-    database: "v7264mir5vsp5sij",
+    user: "maturazi_bd2tab",
+    password: "&m99yv^PMGnV5u",
+    database: "maturazi_bd2tab",
     multipleStatements: true
 }
 
